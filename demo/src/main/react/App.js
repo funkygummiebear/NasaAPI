97fe1b51f8b777ff25ab510d7ba83eaa19959d11
Heather <131703348+funkygummiebear@users.noreply.github.com>
2024-04-21 23:57:11 -0400

    New Changes

--- demo/src/main/react/App.js
+++ demo/src/main/react/App.js
@@ -1,45 +1,29 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
+import AppContainer from "/containers/AppContainer";
 
 export class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            object:"Ball",
+            count: 0
+        }
+    }
 
     render(){
+        const {object, count} = this.state
+        const increment = () => {
+            this.setState({count: this.state.count + 1})
+        }
         return(
-            <>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Features</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Pricing</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <h1>Hello, world!</h1>
-                <div className="mb-3">
-                    <label for="exampleFormControlInput1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-            </>
+            <AppContainer
+                name ="Joe"
+                color="Green"
+                object={object}
+                count={count}
+                increment={increment}
+            />
             )
         }
     }

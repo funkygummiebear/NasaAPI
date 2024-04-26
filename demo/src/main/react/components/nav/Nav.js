77ae4f929cbbd20d40566338fd3d046eb6340247
Heather <131703348+funkygummiebear@users.noreply.github.com>
2024-04-26 09:58:48 -0400

    New Changes

--- demo/src/main/react/components/nav/Nav.js
+++ demo/src/main/react/components/nav/Nav.js
@@ -7,9 +7,7 @@ const Nav = (props) => {
     return(
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        Nasa API
-                    </a>
+                    <a className="navbar-brand" href="/">Nasa API</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -17,16 +15,6 @@ const Nav = (props) => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="currentxkcdcomic">Current
-                                    Comic</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="pastxkcdcomic">Past Comic</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="example">Example</a>
-                            </li>
                             <li className="nav-item">
                                 <a className="nav-link active" href="single">Nasa Single Image</a>
                             </li>
